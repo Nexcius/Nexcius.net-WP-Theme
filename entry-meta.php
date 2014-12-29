721f2979813cb90720653cade0a19fdc80480f0f
Håvard <nex@nexcius.net>
2014-12-29 15:54:23 +0100

    Support for more HTML5 tags

--- entry-meta.php
+++ entry-meta.php
@@ -2,4 +2,5 @@
 <span class="author vcard"><?php the_author_posts_link(); ?></span>
 <span class="meta-sep"> on </span>
 <span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
+<?php edit_post_link("Edit"); ?>
 </section>
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'nexnet' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'nexnet' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'nexnet' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo ' Theme by <a href="http://nexcius.net">nexcius.net</a>'; ?>
 </div>
 </footer>
 </div>
