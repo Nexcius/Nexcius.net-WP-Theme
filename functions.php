cf0a1b129343c7749c140f387f5c333d40dfa554
Håvard <nex@nexcius.net>
2014-12-30 02:34:33 +0100

    Added responsive videos

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+function jetpackme_responsive_videos_setup() {
+    add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'jetpackme_responsive_videos_setup' );
+
 add_action( 'after_setup_theme', 'nexnet_setup' );
 function nexnet_setup()
 {
