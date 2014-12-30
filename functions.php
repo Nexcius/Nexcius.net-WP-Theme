d90f1d12a0fe8c2016fe69943d530768a45d508d
Håvard <nex@nexcius.net>
2014-12-30 03:38:35 +0100

    Added editor style

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function jetpackme_responsive_videos_setup() {
     add_theme_support( 'jetpack-responsive-videos' );
 }
 add_action( 'after_setup_theme', 'jetpackme_responsive_videos_setup' );
-
+add_editor_style();
 add_action( 'after_setup_theme', 'nexnet_setup' );
 function nexnet_setup()
 {
