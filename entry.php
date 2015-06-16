b69cdea105e9c6f392e3f36e79dc1f1aa652339f
Håvard Kindem <havard.kindem@norsk-tipping.no>
2015-06-16 17:01:38 +0200

    Featured image placement. Responsive layout tweaks. Menu optimizations

--- entry.php
+++ entry.php
@@ -1,5 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header>
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+
 <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
 <?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
 </header>
--- page.php
+++ page.php
@@ -3,11 +3,11 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header class="header">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <h1 class="entry-title"><?php the_title(); ?></h1> 
 <section class="entry-meta"><?php edit_post_link("Edit Page"); ?></section>
 </header>
 <section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
